26d05de14d2cd869383a3c77d369ac72c6c9cc08
berwin <berwin7996@gmail.com>
2016-06-04 09:47:05 -0600

    update user.js to emote.js

---
+++ models/emote.js
@@ -0,0 +1,11 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our user schema
+var EmoteSchema   = new mongoose.Schema({
+  name: String,
+  emote: String
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Emote', UserSchema);
