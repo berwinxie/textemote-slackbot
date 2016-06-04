1a33c67bbaf91962482f627b7f66b951ec11a19e
berwin <berwin7996@gmail.com>
2016-06-04 11:42:53 -0600

    added functionality to add new emotes and checks if already exists. also added ability to list current emotes

--- secret.js
+++
@@ -1,5 +0,0 @@
-var config = {};
-config.mongo_user  = 'bear';
-config.mongo_password = 'taco';
-
-module.exports = config;
--- server.js
+++ server.js
@@ -2,26 +2,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var Emote = require('./models/emote');
-var config = require('./secret');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
-// mongolab vars
-var mongo_user = config.mongo_user;
-var mongo_password = config.mongo_password;
 
-// var mongodburl = 'mongodb://' + mongo_user + ':' + mongo_password + '@ds051873.mlab.com:51873/cs498'
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-// db.on('error', console.error);
-// db.once('open', function() {
-
-// });
 //replace this with your Mongolab URL
 // switch between the two to do local/mlab
 mongoose.connect('mongodb://127.0.0.1:27017/slackbot');
-// mongoose.connect(mongodburl);
 
 
 // Create our Express application
@@ -62,28 +52,81 @@ homeRoute.get(function(req, res) {
 var textFaceRoute = router.route('/textface');
 
 textFaceRoute.post(function(req, res) {
-  console.log(req.body);
-  if (req.body.text !== '' || req.body.text !=== undefined) {
-    Emote.find({'emotion':req.body.text.toLowerCase()}, function(err, emote) {
+  if (req.body.text !== '' && req.body.text !== undefined) {
 
-      // emotion not found
-      if (emote.length === 0) {
-        res.status(200);
-        return res.json({
-          "text": 'Emotion was not found Σ(ﾟДﾟ；)'
-        })
+    // used to check for adding new emotions
+    var splitString = req.body.text.toLowerCase().split(':');
+
+    // if text is 'list', then list the emotions
+    if (req.body.text.toLowerCase() === 'list') {
+      var emotionsList = [];
+      Emote.find({}, function(err, emote){
+        emote.forEach(function(document){
+          if (emotionsList.indexOf(document.emotion) == -1) {
+            emotionsList.push(document.emotion.toString());
+          }  
+        });
+        return res.json({ "text": 'Current list of emotions: ' + emotionsList.join(', ')});
+      });
+    }
+
+    // add emotions if in format of: 'add:emotion:emoji'
+    else if (splitString.length === 3 && splitString[0] === 'add' && splitString[1] !== '' && splitString[2] !== '' && splitString[1] !== undefined && splitString[2] !== undefined) {
+      var emote = new Emote();
+      emote.emotion = splitString[1];
+      // this makes it so if the textface has a : in it, it won't be broken up
+      emote.textface = splitString.slice(2).join(':');
+
+      if (emote.emotion !== undefined && emote.textface !== undefined) {
+        // check if already exists
+        Emote.find({'emotion':emote.emotion, 'textface' : emote.textface}, function(err, emotesList) {
+          if (emotesList.length > 0) {
+            return res.json({"text": "This combination already exists!"})
+          }
+
+          else {
+            emote.save(function(err) {
+              if (err){
+                res.status(404);
+                return res.send(err);
+              }
+              res.status(201);
+              return res.json({ "text": 'Emote for ' + splitString[1] + '(' + splitString.slice(2).join(':') + ') created!'});
+            });
+          }
+        })   
       }
       else {
-        // return random emotion from list
-        var index = Math.floor(Math.random()*emote.length)
-        return res.json({
-          "response_type": "in_channel",
-          "text": emote[index].textface
-        })
+        return res.json({ message: 'Empty params'});
       }
+    }
+
+    else {
+      // look for emotions
+      Emote.find({'emotion':req.body.text.toLowerCase()}, function(err, emote) {
+        // emotion not found
+        if (emote.length === 0) {
+          res.status(200);
+          return res.json({
+            "text": 'Emotion was not found Σ(ﾟДﾟ；). Add a new one by using the format: \'add:emotion:emoji\''
+          })
+        }
+        else {
+          // return random emotion from list
+          var index = Math.floor(Math.random()*emote.length)
+          return res.json({
+            "response_type": "in_channel",
+            "text": emote[index].textface
+          })
+        }
+      });
+    } 
+  }
+  else {
+    return res.json({
+      "text": "no params"
     });
   }
-  
 
 });
 
@@ -94,14 +137,23 @@ textFaceInputRoute.post(function(req, res) {
   emote.emotion = req.body.emotion;
   emote.textface = req.body.textface;
   if (emote.emotion !== undefined && emote.textface !== undefined) {
-    emote.save(function(err) {
-      if (err){
-        res.status(404);
-        return res.send(err);
-      }
-      res.status(201);
-      return res.json({ message: 'Emote for ' + req.body.emotion + '(' + req.body.textface + ') created!'});
-    });
+    // check if already exists
+        Emote.find({'emotion':emote.emotion, 'textface' : emote.textface}, function(err, emotesList) {
+          if (emotesList.length > 0) {
+            return res.json({"text": "This combination already exists!"})
+          }
+
+          else {
+            emote.save(function(err) {
+              if (err){
+                res.status(404);
+                return res.send(err);
+              }
+              res.status(201);
+              return res.json({ "text": 'Emote for ' + emote.emotion + '(' +emote.textface + ') created!'});
+            });
+          }
+        })   
   }
   else {
     return res.json({ message: 'Empty params'});
